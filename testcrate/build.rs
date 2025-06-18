27e3b97248d7f87bc86c35e9683a53cb9e9abd99
Your Name <you@example.com>
2025-06-18 08:51:33 -0400

    support prebuilts again

--- testcrate/build.rs
+++ testcrate/build.rs
@@ -1,7 +1,12 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
+    
+    #[cfg(not(feature = "prebuilt"))]
     lute_src_rs::build_lute(lute_src_rs::LConfig {
         disable_crypto: true,
         ..Default::default()
     });
+
+    #[cfg(feature = "prebuilt")]
+    lute_prebuilts_chooser::integrate();
 } // ssssasshdsdsssfhhss
